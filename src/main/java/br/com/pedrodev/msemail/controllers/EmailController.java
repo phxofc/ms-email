c7e68bad42777c9c496d75ea182a6c5074460a5b
phxofc <phdfrocha99@gmail.com>
2023-11-29 21:49:10 -0300

    add yml, sendEmail service, sendingEmail controller

--- src/main/java/br/com/pedrodev/msemail/controllers/EmailController.java
+++ src/main/java/br/com/pedrodev/msemail/controllers/EmailController.java
@@ -1,8 +1,30 @@
 package br.com.pedrodev.msemail.controllers;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pedrodev.msemail.dtos.EmailDTO;
+import br.com.pedrodev.msemail.models.EmailModel;
+import br.com.pedrodev.msemail.services.EmailService;
+import jakarta.validation.Valid;
 
 @RestController
 public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDTO emailDTO){
+       
+        
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDTO, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
     
 }
--- src/main/java/br/com/pedrodev/msemail/services/EmailService.java
+++ src/main/java/br/com/pedrodev/msemail/services/EmailService.java
@@ -1,14 +1,50 @@
 package br.com.pedrodev.msemail.services;
 
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import br.com.pedrodev.msemail.enums.StatusEmail;
+import br.com.pedrodev.msemail.models.EmailModel;
 import br.com.pedrodev.msemail.repositories.EmailRepository;
+
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
     
-        @Autowired
-        EmailRepository emailRepository;
+    @Autowired
+    EmailRepository emailRepository;
+
+    public EmailModel sendEmail(EmailModel emailModel){
+        emailModel.setSenDateTime(LocalDateTime.now());
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
+        
+            mimeMessageHelper.setFrom(emailModel.getEmailFrom());
+            mimeMessageHelper.setTo(emailModel.getEmailTo());
+            mimeMessageHelper.setSubject(emailModel.getSubject());
+            mimeMessageHelper.setText(emailModel.getText());
+            
+            javaMailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        emailModel.setStatusEmail(StatusEmail.ERROR); 
+           } finally{
+             return emailRepository.save(emailModel);
+           }
+
+       
+
+    }
 
 }
